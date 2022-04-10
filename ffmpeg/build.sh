54fbb00d44bcc10df694c29039356c5c5d31bd23
Johnny <hellojinqiang@gmail.com>
2022-04-10 14:01:31 +0800

    ffmpeg/build.sh

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
-
+#
+## docs:
 # https://gist.github.com/johzzy/d6d3503df2d0b9d0a0d44cc9b7874ed8
 # https://github.com/ksvc/FFmpeg/wiki/hevcpush
-
+#
 ## usage:
 # git clone https://github.com/johzzy/ffmpeg FFmpeg
-# ./build_ffmpeg.sh johnny/colorful
+# ./build.sh johnny/colorful
+#
+## FFmpeg
+# git:(master) git checkout johnny/colorful
+# git:(johnny/colorful) git rebase master
+# branch:
+# -   master
+#     -  johnny/colorful
+# 
+# build
+# ./build.sh master
+#       - a487635b85 ok 
+#       - 072101bd52 ok
+#
+# ./build.sh johnny/colorful
+#       - 0d9b275eb7 ok
+#       - 011c33931a ?
+#
 
-cd FFmpeg
+set -e
+
+curr_dir=$(cd $(dirname $0); pwd)
+cd ${curr_dir}/FFmpeg
 
 branch=${1}
 
@@ -27,8 +48,7 @@ echo $ext_configure
 git status
 git checkout $branch
 
-
-./configure --enable-static --enable-pic --enable-gpl \
+./configure --prefix=$(cd ..; echo $(pwd)/out) --enable-static --enable-pic --enable-gpl \
         --disable-encoders --enable-encoder=aac --enable-encoder=libx264  --enable-libx264  \
         --disable-decoders --enable-decoder=aac --enable-decoder=h264 --enable-decoder=hevc  \
         --disable-demuxers --enable-demuxer=aac --enable-demuxer=mov --enable-demuxer=mpegts \
@@ -37,3 +57,4 @@ git checkout $branch
         --disable-doc ${ext_configure}
 
 make -j8
+make install
